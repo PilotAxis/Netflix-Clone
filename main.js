3f1242608f1f7c5d560b7a8ceb67db946628389a
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-10-06 14:51:07 +0530

    Update index.html with get started button functionality and display success message

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-getStarted = get
+const getStarted = document.getElementById("get-started");
+const displayPara = document.getElementById("display-para");
+
+getStarted.addEventListener('click', hidebutton);
+
+function hidebutton() {
+    console.log("displayed!");
+    displayPara.style.display = "block";
+    displayPara.style.textAlign = "center";
+    displayPara.style.color = "#fff";
+    displayPara.style.marginTop = "20px";
+};
