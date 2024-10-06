2ee106d786ee90045ca28d622ccadfbfefbd4f49
Mohammed Ahmed Majid <mohammedahmedmajid14@gmail.com>
2024-10-06 17:53:57 +0530

    Update index.html and style.css with new sections and styles

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const getStarted = document.getElementById("get-started");
+const getStartedfooter = document.getElementById("get-started-footer");
 const displayPara = document.getElementById("display-para");
+const displayParagraph = document.getElementById("display-paragraph");
 
 getStarted.addEventListener('click', hidebutton);
+getStartedfooter.addEventListener('click', hidemenufooter);
+
 
 function hidebutton() {
     console.log("displayed!");
@@ -9,4 +13,12 @@ function hidebutton() {
     displayPara.style.textAlign = "center";
     displayPara.style.color = "#fff";
     displayPara.style.marginTop = "20px";
+};
+
+function hidemenufooter() {
+    console.log("displayed!");
+    displayParagraph.style.display = "block";
+    displayParagraph.style.textAlign = "center";
+    displayParagraph.style.color = "#fff";
+    displayParagraph.style.marginTop = "20px";
 };
